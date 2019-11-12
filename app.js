6e0a1f936ccb5fd24e5c71803366303ad4a473e8
unknown <tamayoralph24@gmail.com>
2019-11-12 21:50:04 +0800

    Install GraphQL and the GraphQL middleware for koa

--- app.js
+++ app.js
@@ -1,12 +1,47 @@
 const Koa = require('koa');
+const Router = require('@koa/router');
+const mount = require('koa-mount');
+const graphqlHTTP = require('koa-graphql');
+const { buildSchema } = require('graphql');
+
 const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
+const router = new Router();
 
-app.use(bodyParser());
+app.use(bodyParser())
+	.use(router.routes())
+	.use(router.allowedMethods())
+;
 
-app.use(async ctx => {
-	ctx.body = 'Hello Worlds';
+app.use(mount('/api', graphqlHTTP({
+	graphiql: true,
+	schema: buildSchema(`
+		type RootQuery {
+			tasks: [String!]!
+		}
+
+		type RootMutation {
+			createTask(name: String): String
+		}
+
+		schema {
+			query: RootQuery
+			mutation: RootMutation
+		}
+	`),
+	rootValue: {
+		tasks: () => {
+			return ['Prepare ingredients', 'Cook meal'];
+		},
+		createTask: (task) => {
+			return task.name;
+		}
+	}
+})));
+
+router.get('/', (ctx, next) => {
+	ctx.body = 'Hello world';
 });
 
 app.listen(3000);
